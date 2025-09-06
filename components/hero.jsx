920c5f9d7dee0f4bd895d4d067df63f6efe7a8ed
fifthmarauder <eeshashahbaz12345@gmail.com>
2025-09-06 18:34:07 +0500

    Update dashboard UI

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,9 +1,34 @@
+"use client"
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Button } from './ui/button'
 import Image from 'next/image'
 
 const HeroSection = () => {
+
+  const imageRef=useRef();
+   
+  useEffect(()=>{
+
+    const imageElement = imageRef.current;
+
+    const handleScroll = () =>{
+    const scrollPosition = window.scrollY;
+    const scrollThreshold = 100;
+
+    if (scrollPosition>scrollThreshold){
+      imageElement.classList.add("scrolled");
+    }else{
+      imageElement.classList.remove("scrolled")
+    }};
+    window.addEventListener("scroll", handleScroll);
+    return ()=>window.removeEventListener("scroll", handleScroll)
+
+
+  },[])
+
+
   return (
     <div className='pb-20 px-4'>
       <div className='container mx-auto text-center'>
@@ -21,8 +46,8 @@ const HeroSection = () => {
                 <Button size="lg" variant= "outline" className="px-8">Watch Demo</Button>
             </Link>
         </div>
-        <div>
-            <div>
+        <div className='hero-image-wrapper'>
+            <div ref={imageRef} className='hero-image'>
                 <Image src="/banner.jpeg" width = {1200} height = {720} alt="Dashboard preview" className='rounded-lg shadow-2xl border mx-auto'  ></Image>
             </div>
         </div>
