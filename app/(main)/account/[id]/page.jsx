26e293c75910069a413c6f2a9997a66dff4c4249
fifthmarauder <eeshashahbaz12345@gmail.com>
2025-10-06 19:01:23 +0500

    Transaction Chart

--- app/(main)/account/[id]/page.jsx
+++ app/(main)/account/[id]/page.jsx
@@ -1,18 +1,19 @@
-import { getAccountWithTransactions } from '@/actions/accounts'
-import { notFound } from 'next/navigation';
-import React, { Suspense } from 'react'
-import TransactionTable from '../_components/transaction-table';
-import { BarLoader } from 'react-spinners';
+import { getAccountWithTransactions } from "@/actions/accounts";
+import { notFound } from "next/navigation";
+import React, { Suspense } from "react";
+import TransactionTable from "../_components/transaction-table";
+import { BarLoader } from "react-spinners";
+import AccountChart from "../_components/account-chart";
 
-export default async function AccountPage({params}) {
+export default async function AccountPage({ params }) {
   const { id } = await params; // ✅ await params first
   const accountData = await getAccountWithTransactions(id);
 
-  if(!accountData){
-    notFound()
+  if (!accountData) {
+    notFound();
   }
 
-  const {transactions, ...account} = accountData
+  const { transactions, ...account } = accountData;
 
   return (
     <div className="space-y-8 px-5">
@@ -37,19 +38,21 @@ export default async function AccountPage({params}) {
         </div>
       </div>
 
-        {/* Chart Section */}
+      {/* Chart Section */}
 
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <AccountChart transactions={transactions}/>
+       
+      </Suspense>
 
-        {/* Transaction Table */}
-        <Suspense fallback={<BarLoader className="mt-4" width={"100%"} color='#9333ea'/>}>
-            <TransactionTable transactions={transactions}/>
-
-
-        </Suspense>
-
-
-
+      {/* Transaction Table */}
+      <Suspense
+        fallback={<BarLoader className="mt-4" width={"100%"} color="#9333ea" />}
+      >
+        <TransactionTable transactions={transactions} />
+      </Suspense>
     </div>
-)}
-
-
+  );
+}
