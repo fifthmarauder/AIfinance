a50fcd297c64ad87f56d9fd849b64de319e9fac1
fifthmarauder <eeshashahbaz12345@gmail.com>
2025-10-07 18:53:15 +0500

    Monthly Budget

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -4,14 +4,28 @@ import CreateAccountDrawer from '@/components/ui/create-account-drawer'
 import { Plus } from 'lucide-react'
 import React from 'react'
 import AccountCard from './_components/account-card'
+import { getCurrentBudget } from '@/actions/budget'
+import BudgetProgress from '../account/_components/budget.progress'
 
 async function DashboardPage() {
-  const accounts = await getUserAccounts()
+  const accounts = await getUserAccounts();
+  const defaultAccount = accounts?.find((account)=>account.isDefault);
+  let budgetData = null;
+  if(defaultAccount){
+    budgetData = await getCurrentBudget(defaultAccount.id)
+  }
     
 
   return (
-    <div className='px-5'>
+    <div className='space-y-8'>
         {/* Budget Progress */}
+        {defaultAccount &&(
+          <BudgetProgress 
+          initialBudget = {budgetData?.budget}
+          currentExpenses = {budgetData?.currentExpenses || 0}
+          />
+        )}
+
 
 
         {/* Overview */}
