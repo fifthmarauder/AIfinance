528ed8c3eb30218c25d5664af0f49bbcd458e164
fifthmarauder <eeshashahbaz12345@gmail.com>
2025-10-20 18:22:21 +0500

    pie chart in dashboard

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,17 +1,21 @@
-import { getUserAccounts } from '@/actions/dashboard'
-import { Card, CardContent } from '@/components/ui/card'
-import CreateAccountDrawer from '@/components/create-account-drawer'
-import { Plus } from 'lucide-react'
-import React from 'react'
-import AccountCard from './_components/account-card'
-import { getCurrentBudget } from '@/actions/budget'
-import BudgetProgress from '../account/_components/budget.progress'
+import { Suspense } from "react";
+import { getUserAccounts } from "@/actions/dashboard";
+import { getDashboardData } from "@/actions/dashboard";
+import { getCurrentBudget } from "@/actions/budget";
+import  AccountCard from "./_components/account-card";
+import  CreateAccountDrawer  from "@/components/create-account-drawer";
+import { Card, CardContent } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+import DashboardOverview  from "./_components/transaction-overview";
+import BudgetProgress from "../account/_components/budget.progress";
+
 
 
 async function DashboardPage() {
   const accounts = await getUserAccounts();
   const defaultAccount = accounts?.find((account)=>account.isDefault);
   let budgetData = null;
+  const transactions = await getDashboardData();
   if(defaultAccount){
     budgetData = await getCurrentBudget(defaultAccount.id)
   }
@@ -21,7 +25,7 @@ async function DashboardPage() {
     <div className='space-y-8'>
         {/* Budget Progress */}
         {defaultAccount &&(
-          <BudgetProgress 
+          <BudgetProgress
           initialBudget = {budgetData?.budget}
           currentExpenses = {budgetData?.currentExpenses || 0}
           />
@@ -30,6 +34,10 @@ async function DashboardPage() {
 
 
         {/* Overview */}
+        <Suspense fallback={"Loading Overview..."}>
+          <DashboardOverview accounts={accounts} transactions ={transactions || []} />
+
+        </Suspense>
 
         {/* Account grid */}
         <div className='grid gap-4 md:grid-cols-2 lg:grids-cols-3'>
