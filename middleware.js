b3d2d300a276513ec785e94395a40102a149d074
fifthmarauder <eeshashahbaz12345@gmail.com>
2025-10-20 18:26:29 +0500

    bot protection

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+import arcjet, { createMiddleware, detectBot, shield } from '@arcjet/next';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
@@ -6,7 +7,22 @@ const isProtectedRoute = createRouteMatcher([
     "/transaction(.*)"
 ]);
 
-export default clerkMiddleware(async(auth,req)=>{
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules:[
+    shield(
+      {mode:'LIVE'}
+    ),
+    detectBot({
+      mode:"LIVE",
+      allow:[
+        "CATEGORY:SEARCH_ENGINE","GO_HTTP"
+      ]
+    })
+  ]
+})
+
+const clerk =  clerkMiddleware(async(auth,req)=>{
     const {userId} = await auth();
 
     if(!userId && isProtectedRoute(req)){
@@ -16,6 +32,7 @@ export default clerkMiddleware(async(auth,req)=>{
     }
 });
 
+export default createMiddleware(aj,clerk)
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
